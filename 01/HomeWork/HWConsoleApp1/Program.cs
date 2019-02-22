f14408d6ab2057b7f1bf606f00f54fa968554bfe
Anatoliy Apanyuk <apanyuk@gmail.com>
2019-02-22 19:06:59 +0300

    Lesson 01 home work added

---
+++ 01/HomeWork/HWConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace DemoApp1
+{
+	class Program
+	{
+		static void Main(string[] args) //HomeWork Lesson1
+		{
+			Console.WriteLine("Введите имя и нажмите клавишу <ENTER>.");
+			string name = Console.ReadLine();
+			while (name.Length == 0) //checks if a name is entered
+			{
+				Console.WriteLine("Вы не ввели имя. Введите имя и нажмите клавишу <ENTER>.");
+				name = Console.ReadLine();
+			}
+			Console.WriteLine($"\n{name}, подождите 5 секунд.");
+			Thread.Sleep(5000);
+			Console.WriteLine($"\n\nРад познакомиться {name}! \nМеня зовут Анатолий. \n{name}, " +
+				"наберитесь еще немного терпения. \nЧерез 5 секунд мы продолжим.");
+			Thread.Sleep(5000);
+			Console.WriteLine($"\n\nК сожалению, мне надо завершить наш диалог. До новых встреч {name}! \n\nЧтобы закрыть консоль, нажмите любую клавишу.");
+			Console.ReadKey();
+		}
+	}
+}
