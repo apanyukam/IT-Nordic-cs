b7105339855b0615db12e5e9250a64332e65587b
Anatoliy Apanyuk <apanyuk@gmail.com>
2019-02-25 21:40:01 +0300

    Lesson 02 class work

---
+++ 02/ClassWork/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите радиус круга");
+			string radius = Console.ReadLine();
+			float r = float.Parse(radius);
+			double d = Math.Pow(r, 2);
+			double s = Math.PI * d;
+			Console.WriteLine("Площадь круга = "+ s);
+		}
+
+	}
+}
