8efb0fc1ecba887905a603e57f98c19be6e98d96
Anatoliy Apanyuk <apanyuk@gmail.com>
2019-03-10 19:27:50 +0300

    Lesson 05 Home Work

---
+++ 05/Home Work/HWConsoleApp5/HWConsoleApp5/Program.cs
@@ -0,0 +1,76 @@
+﻿//Lesson 05 home work_colors 
+using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+
+namespace HWConsoleApp5_Colors
+{
+	[Flags]
+	enum MyColors
+	{
+		None = 0x0,
+		Black = 0x1,
+		Blue = 0x1 << 1,
+		Cyan = 0x1 << 2,
+		Grey = 0x1 << 3,
+		Green = 0x1 << 4,
+		Magenta = 0x1 << 5,
+		Red = 0x1 << 6,
+		White = 0x1 << 7,
+		Yellow = 0x1 << 8
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+			Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+			Console.InputEncoding = Encoding.Unicode;
+			Console.OutputEncoding = Encoding.Unicode;
+
+			Console.Title = "Home work 05_colors";
+
+			MyColors allColors = MyColors.None | MyColors.Black | MyColors.Blue |
+			    MyColors.Cyan | MyColors.Green | MyColors.Grey | MyColors.Magenta |
+			    MyColors.Red | MyColors.White | MyColors.Yellow;
+			Console.WriteLine($"\nВыберите четыре любимых цвета из списка: {allColors}");
+			try
+			{
+				MyColors favoriteColors = MyColors.None;
+				string[] colorRequestNumber = { "первый", "второй", "третий", "четвертый" };
+				MyColors[] color = new MyColors[4]; // array of selected colors
+				for (int i = 0; i < 4; i++)
+				{
+					Console.Write($"\nВведите {colorRequestNumber[i]} любимый цвет: ");
+					object color1; // temporary variable for user input validation
+					while (true)
+					{
+						if (Enum.TryParse(typeof(MyColors), Console.ReadLine(), true, out color1))
+						{
+							color[i] = (MyColors)color1;
+							break;
+						}
+						else
+							Console.WriteLine("Ошибка!");
+					}
+					Console.WriteLine($"Цвет {colorRequestNumber[i]} выбран: {color[i]}");
+					favoriteColors = favoriteColors | color[i];
+				}
+
+				MyColors notFavoriteColors;
+				notFavoriteColors = allColors ^ favoriteColors;
+				Console.WriteLine($"\nЦвета в палитре Избранное : {favoriteColors}");
+				Console.WriteLine($"\nЦвета не вошедшие в палитру Избранное: {notFavoriteColors}");
+				Console.ReadLine();
+			}
+			catch (Exception e)
+			{
+				Console.Write("Введены некорректные данные: ");
+				Console.WriteLine(e.Message);
+				throw;
+			}
+		}
+	}
+}
---
+++ 05/Home Work/HWConsoleApp5/HWConsoleApp5_Geometry/Program.cs
@@ -0,0 +1,96 @@
+﻿//Lesson 05 home work_geometry
+using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+
+namespace HWConsoleApp5_Geometry
+{
+	class Program
+	{
+		enum Figures : byte
+		{
+			Сircle = 1,
+			EquilateralTriangle = 2,
+			Rectangle = 3
+		}
+		static void Main(string[] args)
+		{
+			Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+			Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+			Console.InputEncoding = Encoding.Unicode;
+			Console.OutputEncoding = Encoding.Unicode;
+
+			Console.Title = "Home work 05_geometry";
+			Console.WriteLine("Программа расчитывает площадь и периметр фигур. " +
+				"\nЗначения округляются до сотых ");
+			try
+			{
+				Console.WriteLine("\nВведите тип фигуры: (1 - круг, 2 - равносторонний треугольник, 3 - прямоугольник) ");
+				if (Enum.TryParse(Console.ReadLine(), out Figures a) != true)
+					throw new Exception("Введено нечисловое значение фигуры");
+				else
+					switch (a)
+					{
+						case Figures.Сircle:
+							Console.WriteLine("Введите диаметр круга: ");
+							if (double.TryParse(Console.ReadLine(), out double diameterCircle))
+							{
+								double areaCircle = Math.Round(((1 / (double)4) * ((Math.PI) * Math.Pow(diameterCircle, 2))), 2);
+								double perimetrCircle = Math.Round((Math.PI * diameterCircle), 2);
+								Console.WriteLine($"Площадь круга = {areaCircle} \nПериметр круга = {perimetrCircle}");
+								break;
+							}
+							else
+							{
+								throw new Exception("Введено нечисловое значение диаметра");
+							}
+						case Figures.EquilateralTriangle:
+							Console.WriteLine("Введите длину стороны равностороннего треуголника: ");
+							if (double.TryParse(Console.ReadLine(), out double sideLengthTriangle))
+							{
+
+								double areaEquilateralTriangle = Math.Round(((Math.Pow(sideLengthTriangle, 2) * Math.Sqrt(3)) / (double)4), 2);
+								double perimetrEquilateralTriangle = Math.Round((3 * sideLengthTriangle), 2);
+								Console.WriteLine($"Площадь равностороннего треугольника = {areaEquilateralTriangle} " +
+									$"\nПериметр равностороннего треуголника = {perimetrEquilateralTriangle}");
+								break;
+							}
+							else
+								throw new Exception("Введено нечисловое значение длины");
+
+						case Figures.Rectangle:
+							Console.WriteLine("Введите ширину прямоугольника: ");
+							if (double.TryParse(Console.ReadLine(), out double widthRectangle))
+							{
+
+								Console.WriteLine("Введите высоту прямоугольника: ");
+
+								if (double.TryParse(Console.ReadLine(), out double heighRectangle))
+								{
+									double areaRectangl = Math.Round((widthRectangle * heighRectangle), 2);
+									double perimetrRectangle = Math.Round((2 * (widthRectangle + heighRectangle)), 2);
+									Console.WriteLine($"Площадь прямоугольника = {areaRectangl}" +
+										$"\nПериметр прямоугольника = {perimetrRectangle}");
+									break;
+								}
+								else
+									throw new Exception("Введено нечисловое значение высоты");
+							}
+							else
+								throw new Exception("Введено нечисловое значение ширины");
+						default:
+							throw new Exception("Введено неверное значение фигуры");
+					}
+				Console.ReadLine();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Ошибка: ");
+				Console.WriteLine(e.Message);
+				throw;
+			}
+		}
+	}
+}
