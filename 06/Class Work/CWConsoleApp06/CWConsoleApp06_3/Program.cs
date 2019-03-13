6a3fa513438b2264ee836fda942376628b163a6b
Anatoliy Apanyuk <apanyuk@gmail.com>
2019-03-13 08:21:16 +0300

    Lesson 06 Class Work

--- 06/Class Work/CWConsoleApp06/CWConsoleApp06_3/Program.cs
+++ 06/Class Work/CWConsoleApp06/CWConsoleApp06_3/Program.cs
@@ -8,20 +8,38 @@ namespace CWConsoleApp06_3
 		{
 			var marks = new[]
 			{
-				new[] { 2, 3, 3, 2, 3},
-				new[] {2, 4, 5, 3},
+				new [] {2, 3, 3, 2, 3},
+				new [] {2, 4, 5, 3},
 				null,
-				new[] {5, 5, 5, 5 },
-				new[] {4}
+				new [] {5, 5, 5, 5},
+				new [] {4}
 			};
-			//for (int i = 0; i < marks[0].Length; i++)
-			//{
-			//	for (int j = 0; j < marks[0].Length; j++)
-			//	{
-			//		double a = 
-			//	}
-			//}
 
+			string message = "The average mark for day #";
+			double allMarksForWeek = 0;
+			int marksCount = 0;
+
+			for (uint i = 0; i < marks.Length; ++i)
+			{
+				if (marks[i] == null)
+				{
+					Console.WriteLine(message + i.ToString() + " N/A");
+					continue;
+				}
+
+				double allMarksForDay = 0;
+				for (uint j = 0; j < marks[i].Length; ++j)
+				{
+					allMarksForDay += marks[i][j];
+				}
+
+				Console.WriteLine(message + i.ToString() + " " + (allMarksForDay / marks[i].Length).ToString());
+
+				allMarksForWeek += allMarksForDay;
+				marksCount += marks[i].Length;
+			}
+
+			Console.WriteLine("The average mark for week is {0}", allMarksForWeek / marksCount);
 		}
 	}
 }
