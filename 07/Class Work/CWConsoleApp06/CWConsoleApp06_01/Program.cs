f351761ee27a35fe4762380c3113899394f5ff9c
Anatoliy Apanyuk <apanyuk@gmail.com>
2019-03-13 21:40:14 +0300

    Lesson 07 Class Work

---
+++ 07/Class Work/CWConsoleApp06/CWConsoleApp06_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CWConsoleApp07_01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("введите два числа: ");
+			double a = double.Parse(Console.ReadLine());
+			 
+			double b = double.Parse(Console.ReadLine());
+
+			Console.WriteLine("Конкатенация: ");
+			Console.WriteLine(a + " * " + b + " = " + a * b);
+
+			Console.WriteLine("форматирование: ");
+			Console.WriteLine("{0:#.##} + {1:#.##} = {2:#.##}", a, b, a+b);
+
+			Console.WriteLine("Инторполяция: ");
+			Console.WriteLine($"{a:#.#} - {b:#.#} = {a - b}");
+
+
+
+		}
+	}
+}
---
+++ 07/Class Work/CWConsoleApp06/CWConsoleApp06_02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CWConsoleApp07_02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var test = "my test string";
+			//Console.WriteLine(test.Substring(8, 7));
+			string[] words = test.Split(" ");
+			foreach (string word in words)
+				Console.WriteLine(word);
+
+
+
+		}
+	}
+}
