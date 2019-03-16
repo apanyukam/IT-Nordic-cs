3315000a5c5bd87ae96cf09331ad39fe9fcf6cdf
Anatoliy Apanyuk <apanyuk@gmail.com>
2019-03-16 15:35:42 +0300

    Independent work

--- 06/Class Work/CWConsoleApp06/CWConsoleApp06_3/Program.cs
+++ 06/Class Work/CWConsoleApp06/CWConsoleApp06_3/Program.cs
@@ -1,45 +1,53 @@
 ﻿using System;
 
-namespace CWConsoleApp06_3
+class Program
 {
-	class Program
+	static void Main()
 	{
-		static void Main(string[] args)
+		// Weekly school marks
+		var marks = new[]
 		{
-			var marks = new[]
+			new [] { 2, 3, 3, 2, 3 }, // Monday (it was a good weekend :)
+			new [] { 2, 4, 5, 3 },    // Tuesday (anyway better than Monday)
+			null,                     // Wednesday (felt sick, stayed at home :( )
+			new [] { 5, 5, 5, 5 },    // Thursday (God mode :)
+			new [] { 4 }              // Friday (a very short day)
+		};
+
+		// Overall average mark by the week
+		int overalMarkWeek = 0;
+
+		// Overall number of marks by the week
+		int overallMarkDay = 0;
+
+		// Iterating through the days
+		for (int dayWeek = 0; dayWeek < marks.Length; dayWeek++)
+		{
+			if (marks[dayWeek] == null)
 			{
-				new [] {2, 3, 3, 2, 3},
-				new [] {2, 4, 5, 3},
-				null,
-				new [] {5, 5, 5, 5},
-				new [] {4}
-			};
-
-			string message = "The average mark for day #";
-			double allMarksForWeek = 0;
-			int marksCount = 0;
-
-			for (uint i = 0; i < marks.Length; ++i)
-			{
-				if (marks[i] == null)
-				{
-					Console.WriteLine(message + i.ToString() + " N/A");
-					continue;
-				}
-
-				double allMarksForDay = 0;
-				for (uint j = 0; j < marks[i].Length; ++j)
-				{
-					allMarksForDay += marks[i][j];
-				}
-
-				Console.WriteLine(message + i.ToString() + " " + (allMarksForDay / marks[i].Length).ToString());
-
-				allMarksForWeek += allMarksForDay;
-				marksCount += marks[i].Length;
+				Console.WriteLine($"The average mark for day {dayWeek} is N/A");
+				continue;
 			}
 
-			Console.WriteLine("The average mark for week is {0}", allMarksForWeek / marksCount);
+			// Iterating through the marks during that day
+			int dailySum = 0;
+			for (int markIndex = 0; markIndex < marks[dayWeek].Length; markIndex++)
+			{
+				dailySum += marks[dayWeek][markIndex];
+			}
+
+			Console.WriteLine("The average mark for day {0} is {1:0.0}",
+				dayWeek,
+				(float)dailySum / marks[dayWeek].Length);
+
+			overalMarkWeek += dailySum;
+			overallMarkDay += marks[dayWeek].Length;
 		}
+
+		Console.WriteLine("The average mark for all the week is {0:0.0}",
+			(float)overalMarkWeek / overallMarkDay);
+
+		Console.WriteLine("Press any key to exit...");
+		Console.ReadKey();
 	}
 }
