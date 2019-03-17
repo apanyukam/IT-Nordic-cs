075828decf3cc1d35cb18acacb6af4224086e745
Anatoliy Apanyuk <apanyuk@gmail.com>
2019-03-18 00:04:09 +0300

    Lesson 06 Home Work

---
+++ 06/Home Work/HWConsoleApp06/HWConsoleApp06_bank_deposit/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+namespace HWConsoleApp06_bank_deposit
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+			Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+			Console.InputEncoding = Encoding.Unicode;
+			Console.OutputEncoding = Encoding.Unicode;
+
+			try
+			{
+				double deposit = 0;
+				while (true)
+				{
+					Console.Write("Введите первоначальный вклад: ");
+					deposit = double.Parse(Console.ReadLine());
+					if (deposit <= 0)
+					{
+						Console.WriteLine("Ошибка! Необходимо ввести число больше 0: ");
+						continue;
+					}
+					break;
+				}
+
+				Double bankRate = 0;
+				while (true)
+				{
+					Console.Write("Введите ежедневный процент дохода в виде десятичной дроби (0,03% = 0.0003): ");
+					bankRate = double.Parse(Console.ReadLine());
+					if (bankRate <= 0)
+					{
+						Console.WriteLine("Ошибка! Необходимо ввести число больше 0: ");
+						continue;
+					}
+					break;
+
+				}
+
+				double accumulation = 0;
+				while (true)
+				{
+					Console.Write("Введите желаеммую сумму накопления: ");
+					accumulation = double.Parse(Console.ReadLine());
+					if (accumulation <= deposit)
+					{
+						Console.WriteLine("Ошибка! Сумма накопления должна быть больше первоночального вклада");
+						continue;
+
+					}
+					break;
+				}
+
+				double calculationAccumulation = deposit;
+				uint indexDays = 0;
+				while (calculationAccumulation <= accumulation)
+				{
+					calculationAccumulation += deposit * bankRate;
+					indexDays++;
+				}
+				Console.WriteLine($"Необходимое количество дней для накопления желаемой суммы: {indexDays}");
+
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Ошибка: {e}");
+				throw;
+			}
+		}
+	}
+}
+
---
+++ 06/Home Work/HWConsoleApp06/HWConsoleApp06_natural_number/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+namespace HWConsoleApp06_naturalNumber
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+			Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+			Console.InputEncoding = Encoding.Unicode;
+			Console.OutputEncoding = Encoding.Unicode;
+			int comparisonNamber = 2000000000;
+			int userNumber = 0;
+			uint quantityEvenNumbers = 0;
+
+			Console.WriteLine($"Введите натуральное число не более {comparisonNamber}:");
+			string stringNumber = Console.ReadLine();
+			while (true)
+			{
+				try
+				{
+					userNumber = int.Parse(Console.ReadLine());
+
+					if (userNumber > 0 && userNumber <= comparisonNamber)
+						break;
+					Console.WriteLine($"Ошибка! Введите число натуральное число не больше {comparisonNamber}:");
+				}
+				catch (FormatException e)
+				{
+					Console.WriteLine($"Error {e.GetType()}! Try again");
+					continue;
+				}
+				catch (OverflowException e)
+				{
+					Console.WriteLine($"Error {e.GetType()}! Try again");
+					continue;
+				}
+			}
+
+			int indexNaturalNumber = 0;
+			int userNumberCalculation = userNumber;
+			while (userNumberCalculation != 0)
+			{
+				indexNaturalNumber = userNumberCalculation / 10;
+				if (indexNaturalNumber % 2 == 0)
+					quantityEvenNumbers++;
+				userNumberCalculation = (userNumberCalculation - indexNaturalNumber) / 10;
+			}
+			Console.WriteLine(" В числе {0} количество четных чисе: {1}", userNumber, quantityEvenNumbers);
+		}
+	}
+}
