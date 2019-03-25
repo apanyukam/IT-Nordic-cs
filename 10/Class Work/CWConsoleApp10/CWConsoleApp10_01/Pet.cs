ccd207df5432172d5b4db999c2393f0666493767
Anatoliy Apanyuk <apanyuk@gmail.com>
2019-03-25 21:19:31 +0300

    Lesson10 Class Work

---
+++ 10/Class Work/CWConsoleApp10/CWConsoleApp10_01/Pet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CWConsoleApp10_01
+{
+	class Pet
+	{
+		public string Kind;
+		public string Name;
+		public char Sex;
+		public byte Age;
+
+		public string PropertiesString
+		{
+			get
+			{
+				return $"{Name} is a {Kind} ({Sex}) of {Age} years  old.";
+			}
+
+		}
+
+	}
+}
---
+++ 10/Class Work/CWConsoleApp10/CWConsoleApp10_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CWConsoleApp10_01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// первый вариант рекомендуемый
+			Pet a1 = new Pet
+			{
+				Kind = "Dog",
+				Name = "Charli",
+				Age = 2,
+				Sex ='M'
+			};
+			Console.WriteLine(a1.PropertiesString);
+
+			//второй вариант
+			Pet a2 = new Pet();
+			a2.Kind = "Cat";
+			a2.Name = "Barsic";
+			a2.Age = 5;
+			a2.Sex = 'F';
+			Console.WriteLine(a2.PropertiesString);
+
+
+		}
+	}
+}
